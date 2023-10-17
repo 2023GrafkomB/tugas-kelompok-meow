8b93173a422ce92851d15efb74827efa3b0dfac5
naiakpy <114989408+naiakpy@users.noreply.github.com>
2023-10-17 12:53:04 +0700

    Delete Tugas 4.js directory

--- Tugas 4.js/3D Controller/src/constants/avatar.ts
+++
@@ -1,6 +0,0 @@
-const happyIdleUrl = "../assets/animation/HappyIdle.fbx";
-const walkAnimationUrl = "../assets/animation/Walking.fbx";
-const translateSpeed = 0.03;
-const rotateSpeed = 0.05;
-
-export { happyIdleUrl, walkAnimationUrl, translateSpeed, rotateSpeed };
