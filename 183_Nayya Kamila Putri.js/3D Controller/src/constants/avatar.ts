2d0fd1a612a4f4e7d49dafa8064c9dbbd935c9be
naiakpy <114989408+naiakpy@users.noreply.github.com>
2023-10-17 12:55:41 +0700

    Add files via upload

---
+++ 183_Nayya Kamila Putri.js/3D Controller/src/constants/avatar.ts
@@ -0,0 +1,6 @@
+const happyIdleUrl = "../assets/animation/HappyIdle.fbx";
+const walkAnimationUrl = "../assets/animation/Walking.fbx";
+const translateSpeed = 0.03;
+const rotateSpeed = 0.05;
+
+export { happyIdleUrl, walkAnimationUrl, translateSpeed, rotateSpeed };
